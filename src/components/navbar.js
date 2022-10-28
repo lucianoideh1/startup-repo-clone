62dd272b7c7cdf268c7aab70182309fe9ff3ad25
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-28 11:58:51 -0300

    jsx for navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,6 +2,25 @@ import React from "react";
 
 export default function Navbar(){
     return(
-        <h2>Repo</h2>
+        <div>
+        <ul>
+            <li>
+            <h2>Repo</h2>
+            </li>
+            <li>
+            <select name="select">
+        <option value="value1">Features</option>
+        <option value="value2">Features 2</option>
+        <option value="value3">Features 3</option>
+        <option value="value4">Features 4</option>
+        </select>
+         </li>
+            <li>Blog</li>
+            <li>Pricing</li>
+            <li>Teams Pro</li>
+            <li>Teams for Education</li>
+            <li>Careers</li>
+        </ul>
+        </div>
     )
 }
