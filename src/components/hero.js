c06c22f1d3bf77a3d14e2360ba5f92f201d017f0
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-30 13:51:39 -0300

    created a hero component with text and a btn and its not rendered yet

---
+++ src/components/hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Hero()
+{
+    return(
+        <div className="hero-container">
+            <div className="get-started-container">
+            <h3>Code, create and upload all your projects</h3>
+            <p className="get-started-text">User our free collaborative tool to share code with others and without installing anything</p>
+            <button className="get-started-btn">New Repository</button>
+            </div>
+        </div>
+    )
+}
