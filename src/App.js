507f4b4cb9b3850cd2f37cb67e12e0f65fa34c1a
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-30 13:58:47 -0300

    rendered the hero component and children and added basic styling to the parents

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Hero from './components/hero';
 import Navbar from './components/navbar';
 function App() {
   return (
     <div>
      <h1>Hello World</h1>
      <Navbar />
+     <Hero />
     </div>
   );
 }
--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ export default function Hero()
     return(
         <div className="hero-container">
             <div className="get-started-container">
-            <h3>Code, create and upload all your projects</h3>
+            <h2>Code, create and upload all your projects</h2>
             <p className="get-started-text">User our free collaborative tool to share code with others and without installing anything</p>
             <button className="get-started-btn">New Repository</button>
             </div>
