9bbb57fa6a0f988623d879edf14b7a9caacbdd29
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-31 15:11:15 -0300

    added some text to the hero component and added basic stylingin to the new features component and rendered it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
-import Hero from './components/hero';
 import Navbar from './components/navbar';
+import Hero from './components/hero';
+import Features from './components/features';
 function App() {
   return (
     <div>
      <h1>Hello World</h1>
      <Navbar />
      <Hero />
+     <Features />
     </div>
   );
 }
--- src/components/features.js
+++ src/components/features.js
@@ -3,12 +3,27 @@ import React from "react";
 export default function Features(){
     return(
         <section>
-            <span>Features for all skill levels</span>
+            <span className="features-span">Features for all skill levels</span>
             <div className="features-container">
                 <div className="feature-item">
-                    <div>Icon</div>
-                    <span>Feature title</span>
-                    <p>lorem ipsum</p>
+                    <div className="feature-icon">Icon</div>
+                    <span className="feature-title">Feature title</span>
+                    <p className="feature-p">lorem ipsum</p>
+                </div>
+                <div className="feature-item">
+                    <div className="feature-icon">Icon</div>
+                    <span className="feature-title">Feature title</span>
+                    <p className="feature-p">lorem ipsum</p>
+                </div>
+                <div className="feature-item">
+                    <div className="feature-icon">Icon</div>
+                    <span className="feature-title">Feature title</span>
+                    <p className="feature-p">lorem ipsum</p>
+                </div>
+                <div className="feature-item">
+                    <div className="feature-icon">Icon</div>
+                    <span className="feature-title">Feature title</span>
+                    <p className="feature-p">lorem ipsum</p>
                 </div>
             </div>
         </section>
