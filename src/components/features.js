8f4d6cc20f13809918c692c3a4e57eef3adfac42
Luciano Vazquez <vazquezluciano5@gmail.com>
2022-10-31 14:36:05 -0300

    fixed some things in the hero section and created a new component for features, is not rendered yet

---
+++ src/components/features.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Features(){
+    return(
+        <section>
+            <span>Features for all skill levels</span>
+            <div className="features-container">
+                <div className="feature-item">
+                    <div>Icon</div>
+                    <span>Feature title</span>
+                    <p>lorem ipsum</p>
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/components/hero.js
+++ src/components/hero.js
@@ -2,11 +2,12 @@ import React from "react";
 
 export default function Hero()
 {
+   
     return(
         <div className="hero-container">
             <div className="get-started-container">
             <h2>Code, create and upload all your projects</h2>
-            <p className="get-started-text">User our free collaborative tool to share code with others and without installing anything</p>
+            <p className="get-started-text">User our free collaborative tool  in 50+ languages to share code with others and without installing anything</p>
             <button className="get-started-btn">New Repository</button>
             </div>
         </div>
